d355beed32a40a4336185c8dfec283ebf38b0bee
Artefakt-ff <fasqwerty123@mail.ru>
2018-09-15 14:39:48 +0300

    Fix bug with "like" method

--- components/check_like.py
+++ components/check_like.py
@@ -10,13 +10,12 @@ MAX_TIME_OF_LIKE_LIVING = 1  # day
 def check_like(user=None, topic=None):
     try:
         like = TopicLike.objects.get(creator=user, topic=topic)
-        if (like.liked - datetime.now()).days <= 1:
-            print(like.liked, datetime.now())
-            if like.is_active:
+        if (like.liked - datetime.now()).days <= 1 and like.is_active:
                 like.is_active = False
-            else:
+                return like
+        elif not like.is_active:
                 like.is_active = True
-            return like
+                return like
         return False
     except ObjectDoesNotExist:
         like = TopicLike(creator=user, topic=topic)
